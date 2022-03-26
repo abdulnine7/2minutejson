525cf42b65313c0af376b6fa1097ec537adeb90a
abdulnine7 <me@abdul.co.in>
2022-03-26 23:05:54 +0530

    server created

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const util = require('./util')
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Insert json in DB
+app.post('/', async (req, res) => {
+  const collection = await util.loadJSONCollection();
+  const otp = await util.generateOTP();
+  await collection.insertOne({
+    otp: otp,
+    json: req.body.json,
+    createdAt: new Date(),
+
+  });
+  res.status(201).send({ opt: otp });
+});
+
+// Fetch json from DB
+app.get('/:otp', async (req, res) => {
+  const collection = await util.loadJSONCollection();
+  const data = await collection.findOneAndDelete({ otp: parseInt(req.params.otp) });
+  // console.log(data.value);
+  if(data.value == null){
+    res.status(410).send({"error": "invalid otp or timeout."})
+  }else {
+    res.status(200).send(data.value);
+  }
+});
+
+// Handle production
+if (process.env.NODE_ENV === 'production') {
+  // Static folder
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
---
+++ util.js
@@ -0,0 +1,33 @@
+const mongodb = require('mongodb');
+
+// DB Connection
+async function loadJSONCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://2minjson:coolworld@cluster0.do5w4.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  );
+  return client.db('2minDb').collection('data');
+}
+
+// Generate 4 Digit OTP
+async function generateOTP() {
+  const collection = await loadJSONCollection();
+  var otp = null;
+  do {
+    otp = Math.floor((Math.random() * 8999) + 1000); // randon 4 digit no
+  } while (await collection.findOne({ otp: otp }) != null);
+
+  return otp;
+}
+
+// async function test(){
+//   const collection = await loadJSONCollection();
+//   const jsondata = await collection.findOneAndDelete({ otp: 8712});
+//   console.log(jsondata);
+// }
+// test();
+
+module.exports = {
+  loadJSONCollection,
+  generateOTP
+}
