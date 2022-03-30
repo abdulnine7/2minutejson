1168a4efcf8db966cd7c933f4e843e1d03e2ee3c
abdulnine7 <me@abdul.co.in>
2022-03-30 16:14:05 +0530

    url fixed and other minor fixes

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # 2minutejson
 
-![logo](public/2minutejson.svg)
+![logo](public/logo.svg)
 
 ## Description
 
 2MinuteJSON is a free tool to share json data between apps using simple one time password method. The API accepts post request and generates a OTP. The data is stored for 2 minutes on the server DB. Once the 2 minute timer is exhausted or the data is retrived using OTP the data is flushed from the server DB.
 
-
 ## License
 
 [MIT][license] © [Abdul Noushad Sheikh][pwebsite]
-
-
-
-
-
 
 [license]: /LICENSE
 [website]: https://2minutejson.ml "Free JSON Share API"
--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Static folder
+app.use(express.static(__dirname + "/public/"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
@@ -18,29 +21,30 @@ app.post("/", async (req, res) => {
   util.removeOld(); //deleting data older than 2 minute
   const otp = await util.generateOTP();
   const data = await util.insertOne(otp, req.body.json);
-  delete data._id;
+  if (data != null) delete data._id;
 
-  res.status(201).send({ data: data });
+  if (data == null)
+    res
+      .status(503)
+      .send({ error: "unable to process request at this moment." });
+  else res.status(201).send({ data: data });
 });
 
 // Fetch json from DB
-app.get("/:otp", async (req, res) => {
+app.get("/json/", async (req, res) => {
   util.removeOld(); //deleting data older than 2 minute
-  const data = await util.fetchOne(parseInt(req.params.otp));
-  util.removeOne(parseInt(req.params.otp));
+  const data = await util.fetchOne(parseInt(req.query.otp));
+  if (data != null) delete data._id;
+  util.removeOne(parseInt(req.query.otp));
 
   if (data == null) res.status(410).send({ error: "invalid otp or timeout." });
   else res.status(200).send(data);
 });
 
-// Handle production
-if (process.env.NODE_ENV === "production") {
-  // Static folder
-  app.use(express.static(__dirname + "/public/"));
-
-  // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
-}
+// Handle SPA
+app.get(/.*/, (req, res) => {
+  res.redirect(301, req.protocol + "://" + req.get("host"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
